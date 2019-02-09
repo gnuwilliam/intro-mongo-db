dcbb4d65faeb112e109035db36df74e365e7b843
William Oliveira <sudowilliam@gmail.com>
2019-02-09 14:25:15 -0200

    Queries

--- exercises/queries/queries.js
+++ exercises/queries/queries.js
@@ -1,27 +1,30 @@
 const Post = require('./post')
 
 const postByTitle = (title) => {
-
+  return Post.findOne({title: title}).exec()
 }
 
 const postsForAuthor = (authorId) => {
-
+  return Post.find({author: authorId}).exec()
 }
 
 const fullPostById = (id) => {
-  
+  return Post.findById(id).populate('author').exec()
 }
 
 const allPostsSlim = (fieldsToSelect) => {
-  
+  return Post.find().select(fieldsToSelect).exec()
 }
 
 const postByContentLength = (maxContentLength, minContentLength) => {
-  
+  return Post.find({contentLength: {$gt: minContentLength, $lt: maxContentLength}}).exec()
 }
 
 const addSimilarPosts = (postId, similarPosts) => {
-  
+  return Post.findByIdAndUpdate(postId, {
+    $push: {similarPosts: {$each: similarPosts}}
+  }, {new: true})
+  .exec()
 }
 
 module.exports = {
--- test_database.js
+++ test_database.js
@@ -1,21 +1,57 @@
 const mongoose = require('mongoose')
 
 const connect = () => {
-  return mongoose.connect('mongodb://localhost:27017/testdb')
+  return mongoose.connect('mongodb://localhost:27017/testdb', {useNewUrlParser: true})
 }
 
 const student = new mongoose.Schema({
   firstName: {
     type: String,
     required: true
+  },
+  lastName: {
+    type: String,
+    required: true
+  },
+  school: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: 'school'
   }
 }, {timestamps: true})
 
+const school = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  students: Number,
+  staff: Array
+})
+
+const School = new mongoose.model('school', school)
 const Student = new mongoose.model('student', student)
 
 connect()
   .then(async connection => {
-    const student = await Student.create({firstName: 'Whatever Test'})
-    console.log(student)
+    const school = await School.create({
+      name: 'My School',
+      students: 500,
+      staff: ['Dog', 'Cat', 'Find me']
+    })
+    const school2 = await School.create({
+      name: 'Other School',
+      students: 1000,
+      staff: ['Will', 'Someone', 'Find me']
+    })
+
+    const student = await Student.create({
+      firstName: 'Josh', lastName: 'Finder', school: school._id
+    })
+
+    const match = await School.find({
+      staff: {$in: ['Will']}
+    })
+    console.log(match)
   })
   .catch(e => console.error(e))
